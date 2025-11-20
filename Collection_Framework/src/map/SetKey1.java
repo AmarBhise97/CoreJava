784af1124deed2051c225362e9c454f38765370d
Amar bhise <bhiseamarwagholi@gmail.com>
2025-11-20 21:33:05 +0530

    CLF

---
+++ Collection_Framework/src/map/SetKey1.java
@@ -0,0 +1,25 @@
+package map;
+import java.util.Map;
+
+import java.security.KeyStore.Entry;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SetKey1 {
+public static void main(String[]args) {
+	Map<Integer,String> mp = new HashMap<>();
+	mp.put(101, "Amar");
+	mp.put(105, "Rushi");
+	mp.put(104, "Kartik");
+	mp.put(103, "Suresh");
+	mp.put(102, "Ashwin");
+	mp.put(106, "Kartiki");
+	System.out.println(mp);
+	System.out.println(mp.entrySet());
+    for(java.util.Map.Entry<Integer, String> e: mp.entrySet() ) {
+    	System.out.println(e.getKey());
+    	System.out.println(e.getValue());
+    	
+    }
+}
+}
