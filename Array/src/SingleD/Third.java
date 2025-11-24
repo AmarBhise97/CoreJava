becfcfe88b4ba7d699cde1fda4142ee3312b6925
Amar bhise <bhiseamarwagholi@gmail.com>
2025-11-24 22:46:55 +0530

    Arrays

---
+++ Array/src/SingleD/Third.java
@@ -0,0 +1,15 @@
+package SingleD;
+
+public class Third {
+	public static void main(String[]args) {
+		int arr[]=new int[5];
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		arr[3]=40;
+		arr[4]=50;
+		for(int index=0;index<5;index++) {
+			System.out.print(arr[index]+" ");
+		}
+	}
+}
