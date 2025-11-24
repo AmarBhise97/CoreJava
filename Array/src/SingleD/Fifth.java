f94b97e8f533529d5a075401d089417e579c89b4
Amar bhise <bhiseamarwagholi@gmail.com>
2025-11-24 22:50:05 +0530

    Arrays

---
+++ Array/src/SingleD/Fifth.java
@@ -0,0 +1,31 @@
+package SingleD;
+
+import java.util.Scanner;
+
+public class Fifth {
+	public static void main(String[]args) {
+		if(System.out.printf("Amar")!=null) {////////////Without samicolan//////////////
+			
+		}
+	}
+	public static void main3(String[]args) {
+		Scanner sc= new Scanner(System.in);
+		int arr[]=new int[5];
+		System.out.println("Enter the size");
+		int size = sc.nextInt();
+		//int arr1[]=new int[size];
+	}
+	public static void main2(String[]args) {
+		int arr[]=new int[] {1,2,3};
+		for(int index=0;index<3;index++) 
+			System.out.print(arr[index]+"  ");
+	}
+	public static void main1(String[]args) {
+		int arr[]=new int[3];
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		
+		
+	}	
+}
---
+++ Array/src/SingleD/Fourth.java
@@ -0,0 +1,5 @@
+package SingleD;
+
+public class Fourth {
+
+}
