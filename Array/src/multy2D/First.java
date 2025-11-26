99ede7a2a918aef4c3b9e7566cb9625e6551303d
Amar bhise <bhiseamarwagholi@gmail.com>
2025-11-26 21:49:27 +0530

    2D

--- Array/src/multy2D/First.java
+++ Array/src/multy2D/First.java
@@ -1,5 +1,9 @@
 package multy2D;
 
 public class First {
-
+	public static void main(String[] args) {
+		int arr[] = new int[] { 1, 2, 3, 4 };
+		for (int element : arr) {
+			System.out.print(element + " ");
+		}}
 }
