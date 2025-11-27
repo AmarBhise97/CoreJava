211302f08058caaa0041a6f047d38888ca9c8cb7
Amar bhise <bhiseamarwagholi@gmail.com>
2025-11-27 09:35:10 +0530

    java 8 features

---
+++ StreamAPI/src/fifs_Api/Supplier_1.java
@@ -0,0 +1,11 @@
+package fifs_Api;
+
+import java.util.function.Supplier;
+
+public class Supplier_1 {
+public static void main(String[]args) {
+	Supplier<Double> sup = ()->{return Math.random()*10000;};
+	System.out.println(sup.get().intValue());
+	
+}
+}
