a30c9903718c62f7bacbac4751e2f284bb3d556c
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-10 09:04:23 +0530

    java 8 features

--- StreamAPI/src/method_Referance/Second_Consumer.java
+++ StreamAPI/src/method_Referance/Second_Consumer.java
@@ -13,11 +13,19 @@ public class Second_Consumer {
 		
 		Consumer <String> con = Student1:: add;
 		con.accept("ad");
+		
+		Consumer<Integer> con2 = Student1::add1;
+		con2.accept(10);
 	}
 
 }
 class Student1{
 	public static void add (String s) {
 		System.out.println("this is static methid.....");
+	}
+	
+	public static void add1(Integer a) {
+		System.out.println(a.max(100,5000));
+		
 	}
 }
--- StreamAPI/src/method_Referance/Third_Supplier.java
+++ StreamAPI/src/method_Referance/Third_Supplier.java
@@ -3,11 +3,42 @@ package method_Referance;
 import java.util.function.Supplier;
 
 public class Third_Supplier {
-	public static void main(String[]args) {
-		Supplier<Integer> sup = ()->(1+1);
-	System.out.println(sup.get());
-	
-	
+	public static void main(String[] args) {
+
+		/**
+		 * Supplier<Integer> sup = ()->(1+1); System.out.println(sup.get());
+		 */
+
+		Supplier<Long> sup = Demo::add;
+		System.out.println(sup.get());
+		
+		Supplier<Integer> sup1= Demo::add2;
+		System.out.println(sup1.get());
+		
+		Supplier<Double> sup2=Demo::add3;
+		System.out.println(sup2.get());
+
 	}
 
+}
+
+class Demo {
+	public static long add() {
+		double d = Math.random() * 100;
+		System.out.println(d);
+		long l = (long) d * 10;
+		System.out.println(l);
+		return l;
+
+	}
+
+	public static int add2() {
+		int i = Math.min(100, 2);
+		return i;
+	}
+	
+	public static double add3() {
+		double d=Math.max(100.5, 500.6);
+		return d;
+	}
 }
