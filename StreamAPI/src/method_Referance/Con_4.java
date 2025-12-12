ad97ea49d0bdc0e4ddd57073b4b417b4d6e07fb7
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-12 10:32:54 +0530

    java 8 features

---
+++ StreamAPI/src/method_Referance/Con_4.java
@@ -0,0 +1,23 @@
+package method_Referance;
+
+
+
+public class Con_4 {
+	public static void main(String[] args) {
+		i_f<Printer1,String> pri = Printer1::new;
+		System.out.println(pri.add("null pointer").toString());
+
+	}
+
+}
+
+interface i_f<A, B> {
+	A add(B b);
+}
+
+class Printer1{
+	public Printer1(String s) {
+		
+		System.out.println("adddddd....");
+	}
+}
