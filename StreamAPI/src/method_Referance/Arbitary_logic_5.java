19cd478d9dd1919f5cac552a4e03477acc06ea76
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-12 09:55:57 +0530

    java 8 features

--- StreamAPI/src/method_Referance/Arbitary_logic_5.java
+++ StreamAPI/src/method_Referance/Arbitary_logic_5.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Arbitary_logic_5 {
 	public static void main(String[]args) {
-		
+		//////Constructor////////////////////////////
 		String st = new String("hello");
 		List<String> list = Arrays.asList("abc","abcd","abcde");
 		list.stream()
@@ -13,7 +13,7 @@ public class Arbitary_logic_5 {
 		.map(StringBuilder::reverse)
 		.map(StringBuilder::toString)
 		.map(String::toUpperCase)
-		.map(String::new)
+		
 		.map(st::concat)
 		.forEach(System.out::println);
 		
---
+++ StreamAPI/src/method_Referance/Constructor_referance.java
@@ -0,0 +1,11 @@
+package method_Referance;
+
+import java.util.function.Supplier;
+
+public class Constructor_referance {
+	public static void main(String[]args) {
+		Supplier<StringBuffer> str = StringBuffer::new;
+		System.out.println(str.get().append("asfbb").reverse());
+	}
+
+}
