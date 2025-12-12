d0e54366b56e6c89a060b7f2d3296dc9ebb6e7ee
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-12 10:08:03 +0530

    java 8 features'

---
+++ StreamAPI/src/method_Referance/Constructor_fun_2.java
@@ -0,0 +1,23 @@
+package method_Referance;
+
+import java.util.function.Function;
+
+public class Constructor_fun_2 {
+	public static void main(String[]args) {
+		Function<String,Car1> fun = Car1::new;
+		System.out.println(fun.apply("my name is Amar").getlength());
+		
+	}
+
+}
+class Car1{
+	String Demo;
+	
+	public Car1(String s) {
+		
+		this.Demo = s;
+	}
+	public int getlength() {
+		return Demo.length();
+	}
+}
