70553979ed23909d63a5106c4ff972a679b8b8ee
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-15 10:03:18 +0530

    java 8 features

---
+++ StreamAPI/src/Function/Logical_1.java
@@ -0,0 +1,26 @@
+package Function;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Logical_1 {
+	public static void main(String[]args) {
+		List<String> list = Arrays.asList("Aeou","Eadt","Esfe0","Iot","ght","kmn");
+		Integer Square=list.stream().filter(De::check)
+				.map(StringBuffer::new)
+				.peek(System.out::println)
+				.map(StringBuffer::reverse)
+				.peek((i)->System.out.println(i.length()))
+			    .map(StringBuffer::toString)
+			    .map(String::length)
+				.reduce(0,(i,j)->(i+(j*j)));
+		         System.out.println(Square);
+		
+	}
+
+}
+class De{
+	public static boolean check(String s) {
+		return s.startsWith("A")|| s.startsWith("E")||s.startsWith("I");
+	}
+}
