0955342ee64e8415bfd3ffb2c32455d74a6c11a6
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-15 10:39:36 +0530

    java 8 features

---
+++ StreamAPI/src/optional/Op_Nullable_2.java
@@ -0,0 +1,14 @@
+package optional;
+
+import java.util.Optional;
+
+public class Op_Nullable_2 {
+	public static void main(String[]args) {
+		Optional<String> op = Optional.ofNullable("Am");
+		op.ifPresent(System.out::println);
+		
+		op.ifPresentOrElse((i)->{System.out.println(i.toUpperCase());}
+		, ()->{System.out.println("Not an valid");});
+	}
+
+}
--- StreamAPI/src/optional/Optional_ofnullable.java
+++ StreamAPI/src/optional/Optional_ofnullable.java
@@ -4,11 +4,18 @@ import java.util.Optional;
 
 public class Optional_ofnullable {
 	public static void main(String[]args) {
-		Optional<String> op = Optional.ofNullable(null);
+		Optional<String> op = Optional.ofNullable("Amar");
 		System.out.println(op.isEmpty());
 		System.out.println(op);
-		
 		System.out.println(op.get());
+		
+		System.out.println("=================================================");
+		
+		Optional<String> op1 = Optional.ofNullable(null);
+		System.out.println(op1.isEmpty());
+		System.out.println(op1);
+		
+		System.out.println(op1.get());
 	}
 
 }
