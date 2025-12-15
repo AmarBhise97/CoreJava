de5d6b29521983ba8dc4c030496cf05710039ca3
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-15 11:15:01 +0530

    java 8 features

---
+++ StreamAPI/src/optional/Op_Map.java
@@ -0,0 +1,16 @@
+package optional;
+
+import java.util.Optional;
+
+public class Op_Map {
+	public static void main(String[]args) {
+		Optional<String> op = Optional.ofNullable(null);
+		System.out.println(op.map((i)->(i.length())));
+		
+		System.out.println("===========================================");
+		
+		Optional<String> op1 = Optional.ofNullable("Amar");
+		System.out.println(op1.map((i)->(i.length())));
+	}
+
+}
