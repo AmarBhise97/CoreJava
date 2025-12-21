20085f8eb8b2d987fd5d28e3f89c7294da8f3e46
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-21 23:51:12 +0530

    Reverse String

---
+++ Method/src/StringClass/ReverseString.java
@@ -0,0 +1,23 @@
+package StringClass;
+
+public class ReverseString {
+public static void main(String[] args) {
+		
+		String str="I love JAVA";
+//		//"AVAJ evol I"
+//		
+//		   //10
+//		for(int i=str.length()-1;i>=0;i--)
+//		{
+//			System.out.print(str.charAt(i));
+//		}
+		
+		
+		 String str2=str.toUpperCase();
+		str.toLowerCase();
+		//str="hello"; //Reassign
+		System.out.println(str);
+		System.out.println(str2);
+	}
+
+}
