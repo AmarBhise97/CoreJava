59bce906cdf9fcac4c5b2bf3de887bd30ec64492
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-21 22:52:19 +0530

    Core Concept

---
+++ Core/src/Object/Seventh.java
@@ -0,0 +1,43 @@
+package Object;
+
+public class Seventh {
+	 int age2=56;
+		public static void main(String[] args) {
+			
+			int age=45;
+			
+			Aeroplane1 aero=new Aeroplane1();
+			System.out.println(aero.nameaero);
+			
+			Bus bus=new Bus();
+			bus.start();
+			
+			
+			
+			
+			Truck truck=new Truck();
+			System.out.println(truck.nametruck);
+			
+		}
+	}
+
+	class Aeroplane1
+	{
+	 String nameaero="Boeing";	 //instance
+	}
+	class Bus
+	{
+		
+		public void start()
+		{
+			String name="Leyland"; //local
+			System.out.println(name);
+		}
+	}
+	class Truck
+	{
+		String nametruck="Volvo";  //instance
+		
+	}
+
+
