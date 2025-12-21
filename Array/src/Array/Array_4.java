520c7b9082951e393150b6bc06f3f97cd10f6357
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-21 21:54:19 +0530

    Array

---
+++ Array/src/Array/Array_4.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class Array_4 {
+	public static void main(String[] args) {
+		// [1,2,3....100]
+
+		int[] numbers = new int[100];
+
+		for (int i = 0; i < 100; i++) {
+			numbers[i] = i + 1;// 0,1
+			// 1,2
+			// 2,3
+			// 99,100
+			// 50,51
+		}
+
+		System.out.println(numbers[50]);
+
+		System.out.println(numbers[99]);
+
+	}
+
+}
