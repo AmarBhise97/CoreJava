fbc98ff5a17a4801e41bc529e63393481babaea0
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-21 23:06:58 +0530

    DoWhile

---
+++ Core/src/Loop/DoWhile.java
@@ -0,0 +1,24 @@
+package Loop;
+
+public class DoWhile {
+public static void main(String[] args) {
+		
+		
+		
+		//ICBO
+		int i=21; //initialization      ::Initialization
+		while(i<=20)	 //condition
+		{
+			 System.out.println(i); //block
+			 i++;//operation
+		}
+		
+		do      //block    //IBOC
+		{
+			System.out.println(i);
+			i++;    //operation
+		}while(i<=20); //condition
+
+	}
+
+}
