b3d10810c7465e21a31250e523c4d6e06e48baac
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-21 23:05:36 +0530

    Loop

---
+++ Core/src/Loop/FoorLoop_3.java
@@ -0,0 +1,17 @@
+package Loop;
+
+public class FoorLoop_3 {
+	public static void main(String[] args) {
+
+		for (int j = 0; j < 5; j++) {
+
+			for (int i = j; i < 5; i++) {
+			System.out.print(i);
+			}
+			System.out.println();
+
+		}
+
+	}
+
+}
