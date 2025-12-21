d0dcc01c98056eec4db1bb17941af7051e028647
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-21 22:42:47 +0530

    Object

---
+++ Core/src/Object/Third.java
@@ -0,0 +1,32 @@
+package Object;
+
+public class Third {
+public static void main(String[] args) {
+		
+		Library lib=new Library(); //Object
+		
+		System.out.println(lib.no_of_rooms);
+		
+		lib.read();
+
+	}
+
+}
+
+
+ class Library
+ {
+	 int no_of_rooms=10;
+	 
+	 public void read()
+	 {
+		 int i=10;
+		while(i>5)
+		{
+			System.out.println(i);
+			i--;
+		}
+	 }
+	 
+
+}
