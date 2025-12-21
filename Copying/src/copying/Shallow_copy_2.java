288f2cd324020460854c25b9b367a8a6ad5cea38
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-21 10:22:39 +0530

    Copying

---
+++ Copying/src/copying/Shallow_copy_2.java
@@ -0,0 +1,30 @@
+package copying;
+
+public class Shallow_copy_2 {
+	public static void main(String[]args) throws CloneNotSupportedException {
+		Student1 st = new Student1("Ankit",23,88.60);
+		Student1 st2=(Student1)st.clone();
+		System.out.println(st);
+		System.out.println(st2);
+		
+	}
+
+}
+class Student1{
+	String name;
+	int id;
+	double marks;
+	
+	public Student1(String name,int id,double marks) {
+		this.name=name;
+		this.id=id;
+		this.marks=marks;
+		
+	}
+	public Object clone()throws CloneNotSupportedException{
+		return super.clone();
+	}
+	public String toString() {
+		return this.name+" "+ id+" "+ marks;
+	}
+}
