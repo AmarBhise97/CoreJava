ccd7d853c20335fd8ff7a2341f278826e003bcf2
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-21 22:23:17 +0530

    Static

---
+++ Static/src/static_1/Static_3.java
@@ -0,0 +1,37 @@
+package static_1;
+
+public class Static_3 {
+	public static void main(String[] args) {
+
+		Car honda = new Car();
+		honda.price = 1332.45;
+		honda.name = "City";
+
+		Car skoda = new Car();
+		skoda.price = 10000;
+		skoda.name = "Rapid";
+
+//		   
+//		   honda.display();
+//		   skoda.display();
+
+		System.out.println(honda.name);
+		System.out.println(skoda.name);
+
+		skoda.display();
+		honda.display();
+	}
+
+}
+
+class Car {
+	String name;
+	double price;
+
+	public static void display() {
+		System.out.println("Carrrrrrrrrrrrrrr.....");
+		// System.out.println(name);
+
+	}
+
+}
