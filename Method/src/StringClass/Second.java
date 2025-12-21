1158506645c5fe23d014bd227844c4ddf1b00a35
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-21 23:46:29 +0530

    String

---
+++ Method/src/StringClass/Second.java
@@ -0,0 +1,22 @@
+package StringClass;
+
+public class Second {
+	public static void main(String[] args) {
+
+		String str = "      welcome to India              ";
+
+		System.out.println(str.length());
+		System.out.println(str + 100);
+		System.out.println(str.trim() + 100);
+		System.out.println(str.length());
+
+		String name = "Amar";
+
+		String name2 = "Suresh";
+
+		System.out.println(name.startsWith("A"));
+		System.out.println(name.endsWith("E"));
+
+	}
+
+}
