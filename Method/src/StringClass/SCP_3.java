49e6b3f20da8ca458f7b1b712f81d45afd308c08
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-21 23:55:22 +0530

    SCP

---
+++ Method/src/StringClass/SCP_3.java
@@ -0,0 +1,13 @@
+package StringClass;
+
+public class SCP_3 {
+	public static void main(String[] args) {
+
+		String str = new String("ABC");
+
+		String str2 = new String("ABC");
+
+		System.out.println(str == str2);
+	}
+
+}
