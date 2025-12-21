70be53d82c165e49a77265891a8e8bf1bf75e013
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-21 23:13:26 +0530

    AccesModi

---
+++ accessmodifiers/src/access/Student.java
@@ -0,0 +1,13 @@
+package access;
+
+
+
+public class Student {
+	public static void main(String[]args) {
+		 Employee e1 = new Employee();
+		 
+		 System.out.println(e1.num1);
+		 e1.m1();
+	 }
+
+}
