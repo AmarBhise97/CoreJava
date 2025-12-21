88259048d432f4b4a92cdd5ac57f8e525ff8e190
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-21 18:27:58 +0530

    Array

---
+++ Array/src/Array/Array_1.java
@@ -0,0 +1,37 @@
+package Array;
+
+public class Array_1 {
+	public static void main(String[] args) {
+		
+		
+//        String name1=
+//		String name2=
+//		String name3=
+//		String name4=
+		
+		String[] names=new String[4];
+		
+		names[3]="AKKKKK";
+		
+		names[2]="abc";
+		
+		
+		names[1]="a";
+		
+		names[0]="null";
+		System.out.println(names[1]);
+		
+		
+		
+		double[] salaries=new double[4];
+		salaries[0]=1233.45;
+		salaries[1]=2000.45;
+		salaries[2]=2390;
+		salaries[3]=133.23;
+				
+		System.out.println(salaries[6]);
+		//System.out.println(names[4]);
+}
+
+
+}
