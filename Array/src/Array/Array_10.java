1c5731e2e9a1c3d111a7794236f6266c4a2dc120
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-21 22:05:56 +0530

    Array

---
+++ Array/src/Array/Array_10.java
@@ -0,0 +1,29 @@
+package Array;
+
+public class Array_10 {
+	public static void main(String[] args) {
+		int [] nums=new int[1100];
+		
+		
+		
+		int sum=0;
+		for(int i=0;i<1100;i++)
+		{
+			nums[i]=i+701;
+			
+		}
+		
+		
+		for(int i=0;i<1100;i++)
+		{
+			if(nums[i]%11==0)
+			{
+				sum=sum-nums[i];
+			}
+		}
+		
+		System.out.println(sum);
+
+	}
+
+}
