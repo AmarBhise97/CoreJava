f04d7dc8ab83313bf900c45331ac2a17e93ee8b0
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-21 23:12:06 +0530

    AccessModi

---
+++ accessmodifiers/src/access/Emp_1.java
@@ -0,0 +1,25 @@
+package access;
+
+
+
+public class Emp_1 {
+	int num1 ;
+	Emp_1(){
+	int num1=14 ;
+	
+	}
+	public void record() {
+		System.out.println(num1);
+	}
+	public static void main(String[]args) {
+		
+		
+		
+	}
+public static void main1(String []args) {
+	Employee e1 = new Employee();
+	System.out.println(e1.num1);
+	e1.m1();
+}
+
+}
---
+++ accessmodifiers/src/access/Employee.java
@@ -0,0 +1,12 @@
+package access;
+
+public class Employee {
+	public int num1=10;
+	public Employee(){
+		
+	}
+	public void m1() {
+		
+	}
+
+}
