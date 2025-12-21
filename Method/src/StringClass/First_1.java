d7c5b9194dabdd85d287ae80931ef59089012509
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-21 23:44:05 +0530

    String

---
+++ Method/src/StringClass/First_1.java
@@ -0,0 +1,22 @@
+package StringClass;
+
+public class First_1 {
+	public static void main(String[] args) {
+
+		String str = "hi this is The Java full stack Class and it is awesome";
+
+		// the/java/class
+
+		boolean result = true;
+		if (str.contains("the") || str.contains("java") || str.contains("class")) {
+			result = false;
+		}
+		if (!result) // result==false
+		{
+			System.out.println("not eligible");
+		} else {
+			System.out.println("welcome to the class.");
+		}
+	}
+
+}
