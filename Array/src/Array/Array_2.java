46b8022c3700897f6a621c6fc94ee25039fc33c2
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-21 21:45:51 +0530

    Array

---
+++ Array/src/Array/Array_2.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class Array_2 {
+public static void main(String[] args) {
+		
+		int[] arr=new int[5];
+		
+		
+		//arr[0]=0;
+		//arr[1]=1;
+		for(int i=0;i<5;i++)
+		{
+			 arr[i]=i+1+2+1;
+			
+			 
+		}
+		
+		System.out.println(arr[3]); //106,106,106
+		
+		
+		
+		
+}	
+
+}
