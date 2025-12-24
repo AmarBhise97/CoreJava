5d4944c50cc457fbb6e0e49a81edfd9e700593f0
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-24 12:27:56 +0530

    Multithreading

---
+++ Multithreading/src/basic/Second.java
@@ -0,0 +1,66 @@
+package basic;
+
+public class Second {
+	public static void main(String[]args) throws InterruptedException {
+		Even even=new Even();
+		Prime prime = new Prime();
+		even.start();
+		prime.start();
+		for (int i=0;i<100;i++) {
+			System.out.println("*");
+			Thread.sleep(1000);
+		}
+		
+		
+	}
+
+}
+class Even extends Thread{
+
+
+	public void run() {
+		for(int i= 0 ;i<100;i++) {
+			if(i%2==0) {
+				System.out.println(i+": Even");
+				try {
+				Thread.sleep(1000);
+				}
+				catch(Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		
+		}
+		
+		
+	}
+	
+}
+class Prime extends Thread{
+
+
+	public void run() {
+		for(int i= 2 ;i<100;i++) {
+			try {
+			Thread.sleep(1000);
+			}
+			catch(Exception ex) {
+				ex.printStackTrace();
+			}
+			boolean flag =false;
+			for(int j=2;j<i;j++) {
+				if(i%j==0) {
+					flag =true;
+					
+				}
+				
+				
+				}
+			if(!flag) {
+				System.out.println("Prime :"+ i);
+			}
+		}
+		
+	}
+	
+}
