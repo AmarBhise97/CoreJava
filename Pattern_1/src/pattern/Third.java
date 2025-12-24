f9981313b6e2c25f97a8f20f0a0accb57e59232b
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-24 23:22:11 +0530

    Pattern

--- Pattern_1/src/pattern/Third.java
+++ Pattern_1/src/pattern/Third.java
@@ -2,6 +2,15 @@ package pattern;
 
 public class Third {
 	public static void main(String[]args) {
+		
+		/**
+		 * 25 24 23 22 21 
+           20 19 18 17 16 
+           15 14 13 12 11 
+           10 9 8 7 6 
+           5 4 3 2 1 
+
+		 */
 		int n = 5;
 		int k = 25;
 		for (int i = n; i >= 1; i--) {
