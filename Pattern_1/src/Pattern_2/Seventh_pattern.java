860d8bfad9b840bb88f2156119914800e9cf9bfb
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-26 21:52:48 +0530

    Pattern

--- Pattern_1/src/Pattern_2/Seventh_pattern.java
+++ Pattern_1/src/Pattern_2/Seventh_pattern.java
@@ -1,19 +1,19 @@
 package Pattern_2;
 
-public class Sixth_Pattern {
+public class Seventh_pattern {
 	public static void main(String[] args) {
 		
-	/** 3 5 7 9 11 
-		5 7 9 11 13 
-		7 9 11 13 15 
-		9 11 13 15 17 
-		11 13 15 17 19 
-
-		*/
+/**     0 1 0 1 0 
+		1 0 1 0 1 
+		0 1 0 1 0 
+		1 0 1 0 1 
+		0 1 0 1 0 
+		
+	*/
 		int n = 5;
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
-				System.out.print((2 * (i + j) - 1) + " ");
+				System.out.print(((i + j) % 2) + " ");
 			}
 			System.out.println();
 		}
