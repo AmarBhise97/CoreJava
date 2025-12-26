ccdcafd778f2d7f5a7c959b7d80b6ce89c969690
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-26 21:32:07 +0530

    Pattern

---
+++ Pattern_1/src/Pattern_2/Second.java
@@ -0,0 +1,20 @@
+package Pattern_2;
+
+public class Second {
+	public static void main(String[] args) {
+		int n = 5;
+		int x;
+
+		for (int i = 1; i <= n; i++) {
+			x = i;
+			for (int j = 1; j <= n; j++) {
+
+				System.out.print(x + " ");
+				x += n;
+
+			}
+			System.out.println();
+		}
+	}
+
+}
