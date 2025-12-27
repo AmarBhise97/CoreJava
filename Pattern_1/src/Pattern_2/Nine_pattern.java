c7bb1b0b3d373da6bcc60d9c6b53c3d854ffb727
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-27 22:19:26 +0530

    Pattern

--- Pattern_1/src/Pattern_2/Nine_pattern.java
+++ Pattern_1/src/Pattern_2/Nine_pattern.java
@@ -2,6 +2,13 @@ package Pattern_2;
 
 public class Nine_pattern {
 	public static void main(String[] args) {
+		
+/**		1 0 1 0 1 
+		0 0 0 0 0 
+		1 0 1 0 1 
+		0 0 0 0 0 
+		1 0 1 0 1 
+*/
 		int n = 5;
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
