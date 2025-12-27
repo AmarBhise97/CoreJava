eb1e6628188f4b6f6eb2d3a3fa4cc0ab6cb4c3c9
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-27 22:41:07 +0530

    Pattern

--- Pattern_1/src/Pattern_3/Five_p.java
+++ Pattern_1/src/Pattern_3/Five_p.java
@@ -2,6 +2,13 @@ package Pattern_3;
 
 public class Five_p {
 	public static void main(String[] args) {
+		
+	/**	A A A A A 
+		B B B B B 
+		C C C C C 
+		D D D D D 
+		E E E E E 
+*/
 		char i;
 		char j;
 		for( i='A';i<='E';i++) {
---
+++ Pattern_1/src/Pattern_3/Five_pt.java
@@ -0,0 +1,15 @@
+package Pattern_3;
+
+public class Five_pt {
+	public static void main(String[] args) {
+		char i;
+		char j;
+		for(i='A';i<='E';i++) {
+			for(j='A';j<='E';j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
