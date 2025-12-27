e2d445ce42db9d47ccb08cd3ef4b25010e377ccb
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-27 22:17:30 +0530

    Pattern

--- Pattern_1/src/Pattern_2/Eight_pattern.java
+++ Pattern_1/src/Pattern_2/Eight_pattern.java
@@ -2,6 +2,13 @@ package Pattern_2;
 
 public class Eight_pattern {
 	public static void main(String[] args) {
+		
+/**		1 0 1 0 1 0 
+		0 1 0 1 0 1 
+		1 0 1 0 1 0 
+		0 1 0 1 0 1 
+		1 0 1 0 1 0 
+*/
 		int n = 5;
 		for (int i = 1; i <= n; i++) {
 			for (int j = 0; j <= n; j++) {
---
+++ Pattern_1/src/Pattern_2/Nine_pattern.java
@@ -0,0 +1,14 @@
+package Pattern_2;
+
+public class Nine_pattern {
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				System.out.print((i * j) % 2 + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
