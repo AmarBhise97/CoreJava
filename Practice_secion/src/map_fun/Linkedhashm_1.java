cfa445ac00dcbabb2c85da1e70c01b7bf8957779
Amar bhise <bhiseamarwagholi@gmail.com>
2026-01-04 19:41:11 +0530

    Rivision

---
+++ Practice_secion/src/map_fun/Linkedhashm_1.java
@@ -0,0 +1,24 @@
+package map_fun;
+
+import java.util.LinkedHashMap;
+
+public class Linkedhashm_1 {
+	public static void main(String[]args) {
+		LinkedHashMap<Integer,Integer> lm = new LinkedHashMap<>();
+		lm.put(12, 12);
+		lm.put(null, 13);
+		lm.put(null, 9);
+		lm.put(null, 16);
+		lm.put(14, 12);
+		lm.put(18, 12);
+		lm.put(10, 12);
+		System.out.println(lm);
+		System.out.println(lm.pollFirstEntry());
+		System.out.println(lm.pollLastEntry());
+		System.out.println(lm.remove(null, 16));
+		//System.out.println(lm.pollLastEntry());
+		//System.out.println(lm.pollLastEntry());
+		System.out.println(lm);
+	}
+
+}
