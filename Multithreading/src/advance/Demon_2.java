4df59e413e43af92e5e1a01acf7110ddeb698ab7
Amar bhise <bhiseamarwagholi@gmail.com>
2026-01-06 23:13:03 +0530

    Multythreading

---
+++ Multithreading/src/advance/Demon_2.java
@@ -0,0 +1,40 @@
+package advance;
+
+public class Demon_2 {
+	public static void main(String[]args) {
+		Demo2 demo = new Demo2();
+		Demo3 de = new Demo3();
+		
+		demo.setDaemon(true);
+		demo.start();
+		de.setDaemon(true);
+		de.start();
+		
+		System.out.println("hey...");
+	}
+
+}
+class Demo2 extends Thread{
+	int count=2;
+	public void run() {
+		
+		for(int i=0;i<100;i++) {
+			System.out.println(i);
+			count++;
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
+class Demo3 extends Thread{
+	public void run() {
+		for(int i=0;i<10;i++) {
+			System.out.println(i+"*");
+		}
+	}
+}
