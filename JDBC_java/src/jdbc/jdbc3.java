0482d074c72ad7dcf00c349a953af52c63f5f1e9
Amar bhise <bhiseamarwagholi@gmail.com>
2026-01-12 22:13:16 +0530

    batch jdbc

---
+++ JDBC_java/src/jdbc/jdbc3.java
@@ -0,0 +1,43 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class jdbc3 {
+	public static void main(String[]args)throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc","root","9730");
+		
+		Statement st = con.createStatement();
+		String first = "insert into student values(10,'Amar');";
+		String second = "insert into student value(11,'ankit');";
+		String third = "insert into student value(10,'gagan');";
+		
+		st.addBatch(first);
+		st.addBatch(second);
+		st.addBatch(third);
+		
+		int arr[]=st.executeBatch();
+		
+		for(int i:arr) {
+			System.out.println(i);
+		}
+		
+		
+		st.close();
+		con.close();
+		
+	}
+
+}
+class student{
+	int id;
+	String name;
+	public student(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+}
