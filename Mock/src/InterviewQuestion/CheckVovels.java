23fdde97469cf1844973d245d2416971e1561b04
Amar bhise <bhiseamarwagholi@gmail.com>
2026-03-08 14:17:17 +0530

    MOCK

---
+++ Mock/src/InterviewQuestion/CheckVovels.java
@@ -0,0 +1,23 @@
+package InterviewQuestion;
+
+public class CheckVovels {
+	
+	public static void main(String[]args) {
+		
+		String str="Java Programing";
+		int counter=0;
+		
+		for(int i=0;i<str.length();i++) {
+			char ch=str.charAt(i);
+			if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+				counter++;
+				System.out.println(ch);
+				
+			}
+			
+		}
+		System.out.println(counter );
+		
+	}
+
+}
