31045b3508c82640fa8cb9b691259457b8f1c661
Amar bhise <bhiseamarwagholi@gmail.com>
2026-03-27 18:33:57 +0530

    Interview Questions:

---
+++ Mock/src/InterviewQuestion/LongestSubstring.java
@@ -0,0 +1,33 @@
+package InterviewQuestion;
+
+import java.util.HashSet;
+
+public class LongestSubstring {
+	
+
+	    public static void main(String[] args) {
+	        String str = "abcabcbb";
+	        int result = lengthOfLongestSubstring(str);
+
+	        System.out.println("Length: " + result);
+	    }
+
+	    public static int lengthOfLongestSubstring(String s) {
+	        HashSet<Character> set = new HashSet<>();
+	        int left = 0, maxLength = 0;
+
+	        for (int right = 0; right < s.length(); right++) {
+	            while (set.contains(s.charAt(right))) {
+	                set.remove(s.charAt(left));
+	                left++;
+	            }
+
+	            set.add(s.charAt(right));
+	            maxLength = Math.max(maxLength, right - left + 1);
+	        }
+
+	        return maxLength;
+	    }
+	
+
+}
