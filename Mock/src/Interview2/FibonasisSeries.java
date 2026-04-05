18b1a28a7afe606612b6baf5c6217f12198a4984
Amar bhise <bhiseamarwagholi@gmail.com>
2026-04-05 21:22:47 +0530

    Fibanosis

---
+++ Mock/src/Interview2/FibonasisSeries.java
@@ -0,0 +1,17 @@
+package Interview2;
+
+public class FibonasisSeries {
+
+	public static void main(String[] args) {
+		int n = 10, a = 0, b = 1;
+
+		for(int i = 0; i < n; i++){
+		    System.out.print(a + " ");
+		    int c = a + b;
+		    a = b;
+		    b = c;
+		}
+
+	}
+
+}
