8b4941242969b5e5392ccea52071fce9ba4caab2
Amar bhise <bhiseamarwagholi@gmail.com>
2026-04-11 21:33:01 +0530

    Remove duplicate

---
+++ Mock/src/Interview2/RemoveDuplicate.java
@@ -0,0 +1,20 @@
+package Interview2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RemoveDuplicate {
+
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 2, 3, 4, 4};
+		Set<Integer> set = new HashSet<>();
+
+		for(int num : arr){
+		    set.add(num);
+		}
+
+		System.out.println(set);
+
+	}
+
+}
